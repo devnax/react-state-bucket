3e3dafbe793ef276f8260b8133f934b9f393cc35
Naxrul Ahmed <najrulahmed@Najruls-MacBook-Pro.local>
2025-05-26 22:03:27 +0600

    updated makepack

--- main.tsx
+++ main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { createBucket } from './pack';
+import { createBucket } from './src';
 
 const useForm = createBucket({
   name: ""
